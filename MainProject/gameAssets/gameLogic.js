c04fc304b09f52b70aadcf93a637f3b86760be45
aMononito <pedro@torstig.us>
2025-01-10 11:34:57 -0500

    commit Logic gameAssets

---
+++ MainProject/gameAssets/gameLogic.js
@@ -0,0 +1,12 @@
+const dealButton = document.querySelector('#dealButton');
+const newGame = document.querySelector('#new-game');
+const cpuDeck = document.querySelector('#computer-deck');
+const cpuPlay = document.querySelector('#cpuPlay');
+const cpuCount = document.querySelector('#cpuCount');
+const userDeck = document.querySelector('#userDeck');
+const userPlay = document.querySelector('#userPlay');
+const userCount = document.querySelector('#userCount');
+const winLossMsg = document.querySelector('#winLossMsg');
+const cardTotal = Array()
+
+console.log(dealButton)
