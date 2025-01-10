935f8f148e734166623dd76da52416ccea5c87b0
[Koosha Moradpour] <kushamoradpour@gmail.com>
2025-01-10 17:11:53 -0500

    add more detail on about page

--- assets/Js/about.js
+++ assets/Js/about.js
@@ -8,17 +8,116 @@ container.addEventListener('click', function (event) {
   if (element.matches('.infoBox')) {
     const state = element.getAttribute('data-state');
 
-    // Use an if statement to conditionally render the number on the card
+    
     if (state === 'hidden') {
-      // If the card is clicked while the state is 'hidden', we set .textContent to the number
+      
       element.textContent = element.dataset.info;
-      // Using the dataset property, we change the state to visible because the user can now see the number
+    
       element.dataset.state = 'visible';
     } else {
-      // 'Hide' the number by setting .textContent to an empty string
-      element.textContent = '';
-      // Use .setAttribute() method
+     
+      element.textContent = 'Please click here:';
+   
       element.setAttribute('data-state', 'hidden');
     }
   }
 });
+
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const playerOptions = [rockBtn, paperBtn, scissorBtn];
+        const computerOptions = ['rock', 'paper', 'scissors']
+
+        playerOptions.forEach(option => {
+            option.addEventListener('click', function () {
+
+                const movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${10 - moves}`;
+
+
+                const choiceNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[choiceNumber];
+
+                winner(this.innerText, computerChoice)
+                if (moves == 10) {
+                    gameOver(playerOptions, movesLeft);
+                }
+            })
+        })
+
+    }
+
+ 
+    const winner = (player, computer) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+
+        if (player === "rock" && computer === "scissors" ||
+            player === "scissors" && computer === "paper" ||
+            player === "paper" && computer === "rock" ){
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+        
+        }else if (player === computer){
+            result.textContent = 'Tie'
+            
+        }else {
+            result.textContent = 'Computer Won';
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+            
+        }
+    }
+
+    const gameOver = (playerOptions, movesLeft) => {
+
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+
+
+        chooseMove.innerText = 'Game Over!!'
+        movesLeft.style.display = 'none';
+
+        if (playerScore > computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Won The Game'
+            result.style.color = '#308D46';
+        }
+        else if (playerScore < computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        }
+        else {
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey'
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex'
+        reloadBtn.addEventListener('click', () => {
+            window.location.reload();
+        })
+    }
+    playGame();
+
+}
+game();
