ba92cf02baba00e7ddd9327950bbe71998e28bd5
Liv-5 <osmith5@kent.edu>
2025-01-13 12:57:23 -0500

    work

--- assets/Js/war.js
+++ assets/Js/war.js
@@ -1,10 +1,17 @@
 const dealBtn = document.getElementById("deal");
 const newGameBtn = document.getElementById("new-game");
 const modal = document.querySelector(".modal");
+const modalInput = document.getElementById("your-name");
+const submitBtn = document.getElementById("submit-btn");
+const displayArea = document.getElementById("display-area");
 
-newGameBtn.onclick = function () {
-  modal.style.display = "block";
-};
+submitBtn.addEventListener("submit", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  const inputValue = modalInput.value;
+  console.log("inputValue", inputValue);
+  displayArea.textContent = inputValue;
+});
 
 const newCard = function () {
   Math.floor(Math.random() * 26) + 1;
@@ -12,4 +19,9 @@ const newCard = function () {
 
 dealBtn.addEventListener("click", newCard);
 
-append to change player name 
+console.log("hello");
+
+$(document).on("click", "#btn-submit", function (event) {
+  event.preventDefault();
+  console.log("hello");
+});
