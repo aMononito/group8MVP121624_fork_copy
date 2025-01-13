c3429350b51ad8ed56ba043d462ffde02c0051ac
Liv-5 <osmith5@kent.edu>
2025-01-13 16:07:13 -0500

    linked another js file, finished winner js

--- assets/Js/war.js
+++ assets/Js/war.js
@@ -1,10 +1,11 @@
-const dealBtn = document.getElementById("deal");
+const dealButton = document.querySelector("#dealButton");
 const newGameBtn = document.getElementById("new-game");
 const modal = document.querySelector(".modal");
 const modalInput = document.getElementById("your-name");
 const submitBtn = document.getElementById("submit-btn");
 const displayArea = document.getElementById("display-area");
 const formEl = document.getElementById("form");
+const winner = document.getElementById("winner-display");
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,10 +15,8 @@ formEl.addEventListener("submit", (event) => {
   displayArea.textContent = inputValue;
 });
 
-// if ()
-
-// const newCard = function () {
-//   Math.floor(Math.random() * 26) + 1;
-// };
-
-// dealBtn.addEventListener("click", newCard);
+if (userScore.value === 0) {
+  winner.textContent = "Computer Wins!";
+} else if (cpuScore.value === 0) {
+  winner.textContent = `${inputValue} Wins!`;
+}
